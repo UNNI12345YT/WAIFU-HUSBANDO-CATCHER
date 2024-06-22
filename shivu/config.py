e8e355f579cad95ba2ebe593764b21de37b50c30
82kpro_Official <161997419+UNNI12345YT@users.noreply.github.com>
2024-06-22 21:05:02 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -2,18 +2,18 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    OWNER_ID = "6765826972"
-    sudo_users = "6845325416", "6765826972"
-    GROUP_ID = -1002133191051
-    TOKEN = "6707490163:AAHZzqjm3rbEZsObRiNaT7DMtw_i5WPo_0o"
+    OWNER_ID = "6171681404"
+    sudo_users = "6171681404"
+    GROUP_ID = -1002110035934
+    TOKEN = "7478405221:AAHh-mBrzPFpx0FNN1N8J8FDj8KrcY_jMvY"
     mongo_url = "mongodb+srv://HaremDBBot:ThisIsPasswordForHaremDB@haremdb.swzjngj.mongodb.net/?retryWrites=true&w=majority"
     PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
-    SUPPORT_CHAT = "Collect_em_support"
-    UPDATE_CHAT = "Collect_em_support"
-    BOT_USERNAME = "Collect_Em_AllBot"
-    CHARA_CHANNEL_ID = "-1002133191051"
-    api_id = 26626068
-    api_hash = "bf423698bcbe33cfd58b11c78c42caa2"
+    SUPPORT_CHAT = "HAPPY_SUPPORT_GRP"
+    UPDATE_CHAT = "HAPPY_SUPPORT_GRP"
+    BOT_USERNAME = "Unni_secure_bot"
+    CHARA_CHANNEL_ID = "-1002237242985"
+    api_id = 27408015
+    api_hash = "2f07e7c921c8d2b982df12d65a46ca46"
 
     
 class Production(Config):
